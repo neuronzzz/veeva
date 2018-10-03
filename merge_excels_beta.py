8ca8cdd4cc32afeb2a12ea6f0d2affc2f4b37b19
neuronzzz <zhangyuspirit@icloud.com>
2018-10-03 17:07:00 +0800

    1. fix all the bugs in script 2. add readme comments to merge_excels_beta.py

--- merge_excels_beta.py
+++ merge_excels_beta.py
@@ -7,8 +7,14 @@ from pylev import levenshtein
 from sklearn.cluster import DBSCAN
 
 
-# 需求：合并文档，并合并重复的column。重复column根据编辑距离来计算，认为编辑距离小于1的就是重复的。合并的column要sum，并放在一个新column。
-# 特殊问题：文件中有空的隐藏行需要去掉。
+# 需求：合并文档
+# 问题：
+# 1. 合并的文档，可能存在客户自己修改了column name，但是通常情况下，只会修改错误某个字符，比如:'2018 Q4'，修改成了'2018 Q3'，或者
+# '1018 Q4'，这种问题，用编辑距离<1来衡量column是否是一样的
+# 2. 对于因为客户修改，导致出来多个column，本质上是同一个column，需要做合并处理到一个column，并且，里面的值要做sum，并放在一个新column
+# 3. 对于数值的cell，可能存在nan，也就是没有任何值
+# 4. 对于本来是数值的cell，可能存在是字符型的cell
+# 5. 特殊问题：文件中有空的隐藏行需要去掉
 
 def lev_metric(x, y):
     i, j = int(x[0]), int(y[0])  # extract indices
@@ -27,17 +33,21 @@ if __name__ == '__main__':
 
     for file_name in glob.glob(directory):
         # df = df.append(pd.read_excel(file_name, sheetname=sheet), ignore_index=True)
-        # for pd.ead_excel, if do not specify a sheet_name default will read the first sheet. this will be useful
-        # especially if not clear about the name of first sheet but know only one sheet of first sheet is the
-        # sheet want to use
+        # 对于pd.read_excel, 如果没有设置sheet_name，会读取第一个sheet，或者读取所有？如果对excel的处理就是处理第一个，那么不设置值
+        # 比较好，如果设置值，值如果变化了，程序，或者文件必须要进行修改保持一致
         df_part = pd.read_excel(file_name)
         # 下面这个过滤的过程，可以看出DataFrame，仍然是按照行索引来过滤行，里面是用来过滤掉响应的索引，得到索引之后来获取行
+        # 过滤过程相当于[]中括号内部，传入一个条件，根据这个条件来过滤。本质上，这个条件返回的是一个布尔索引（series with True/False value），
+        # 根据这个索引来决定哪些row保留
         # 过滤null的几种方法：https://stackoverflow.com/questions/22551403/python-pandas-filtering-out-nan-from-a-data-selection-of-a-column-of-strings
         df_part = df_part[df_part[col_ID_18].notnull()]
-        duplicated_columns = np.hstack((df.columns.values, df_part.columns.values))
+        # 下面是对column进行合并，并去重处理，利用了set，但是set会把list顺序打乱，所以要保存list的index，合并之后利用这个index重新排序
+        # 下面df_part与df的顺序不能调换，因为df的构建有一个append操作，如果append的dataframe中有df没有个column，df在append之后
+        # column顺序就会改变
+        duplicated_columns = np.hstack((df_part.columns.values, df.columns.values))
         union_columns = list(set(duplicated_columns))
         union_columns.sort(key=duplicated_columns.tolist().index)
-        df = df.append(df_part, ignore_index=True)[union_columns]
+        df = df.append(df_part, ignore_index=True)
 
     # 下面这个主要用来保持dataframe的column与原始的一致，因为如何两个excel column不一致，append的时候，会重新按照A-Za-z把column排序
     df = df[union_columns]
@@ -46,19 +56,17 @@ if __name__ == '__main__':
     column_names = df.columns.values
     print(gp.count()[col_ID_18])
 
-    # writer = pd.ExcelWriter(output_doc)
-    # df.to_excel(writer, sheet, index=False)
-    # writer.save()
-
+    # 可以用exit()函数来退出脚本的执行
     # exit()
 
     # cluster column names base on the edit distance
     X = np.arange(len(column_names)).reshape(-1, 1)
     db = DBSCAN(eps=1, min_samples=1, metric=lev_metric).fit(X)
-    # labels_ is the cluster
+    # labels_ is the cluster, 结果是X对应的cluster，例如X行数是10，结果可能是[0，0，0，1，2，3，4，5，6，7]，表示前三个是cluster 0
     print(db.labels_)
 
     # solution1: build dictionary, cluster is the key and merge columns to list as the value
+    # 下面这个方案是把DBSCAN计算得到的cluster转成dictionary，key是cluster，value是X的索引
     # https://www.cnblogs.com/herbert/archive/2013/01/09/2852843.html
     # https://www.tutorialspoint.com/How-to-create-Python-dictionary-with-duplicate-keys
     d = defaultdict(list)
@@ -67,7 +75,7 @@ if __name__ == '__main__':
 
     d = dict(d)
 
-    # sum columns
+    # 合并在cluster中的column，并且做sum操作来合并
     # ''.join(<list>) 要求list是string类型的list，起码不能是int类型的list，否则回报如下异常
     # TypeError: sequence item 0: expected str instance, int found
     for k, v in d.items():
@@ -75,6 +83,12 @@ if __name__ == '__main__':
             df_group = df[v]
             df[' & '.join(df_group.columns)] = df_group.fillna(0).astype(float).sum(axis=1)
             df = df.drop(df.columns[v], axis=1)
+            # try:
+            #     df_group = df[v]
+            #     df[' & '.join(df_group.columns)] = df_group.fillna(0).astype(float).sum(axis=1)
+            #     df = df.drop(df.columns[v], axis=1)
+            # except Exception as e:
+            #     raise e
 
     # try:
     #     df[' & '.join(df_group.columns)] = df_group.fillna(0).astype(float).sum(axis=1)
